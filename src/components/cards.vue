d69be76be1408aaa35735d5b64781d052a1695cc
Duckjpeg <Harry.steven@icloud.com>
2023-01-01 20:07:06 +0000

    title is working and scailing :)

---
+++ src/components/cards.vue
@@ -0,0 +1,187 @@
+<template lang="">
+	<div>
+		<nav class="navBar">
+			<div id="cards">
+				<div class="card">
+					<router-link class="bigNavLink" to="/"></router-link>
+					<div class="cardBorder"></div>
+					<div class="cardContent">
+						<div class="cardImgWrapper">
+							<img src="/house.png" alt="" class="home cardImg" />
+						</div>
+						<div class="infoWrapper">
+							<div class="infoTitle">Home</div>
+							<div class="infoDescription"></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<router-link class="bigNavLink" to="#about"></router-link>
+					<div class="cardBorder"></div>
+					<div class="cardContent">
+						<div class="cardImgWrapper">
+							<img src="/info.png" class="about cardImg" />
+						</div>
+						<div class="infoWrapper">
+							<div class="infoTitle">About</div>
+							<div class="infoDescription"></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<router-link
+						class="bigNavLink"
+						to="#projects"
+					></router-link>
+					<div class="cardBorder"></div>
+					<div class="cardContent">
+						<div class="cardImgWrapper">
+							<img
+								src="/blueprint.png"
+								class="projects cardImg"
+							/>
+						</div>
+						<div class="infoWrapper">
+							<div class="infoTitle">pRojects</div>
+							<div class="infoDescription"></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<router-link class="bigNavLink" to="#contact"></router-link>
+					<div class="cardBorder"></div>
+					<div class="cardContent">
+						<div class="cardImgWrapper">
+							<img src="/contact.png" class="contact cardImg" />
+						</div>
+						<div class="infoWrapper">
+							<div class="infoTitle">contAct</div>
+							<div class="infoDescription"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.navBar {
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+#cards {
+	/*display: inline-grid;*/
+	display: none;
+	width: 95vw;
+	width: 95dvw;
+	gap: 1vw;
+	grid-template-columns: repeat(4, 1fr);
+	grid-template-rows: 1fr;
+	padding: 2.5vw;
+}
+#cards:hover > .card > .cardBorder {
+	opacity: 1; /*on hover they all set to clear and set to the coordinate */
+	/*for some reason it doesn't overflow the box */
+	/*doesnt overflow because its a background not its own element numb nuts */
+}
+.card {
+	aspect-ratio: 5/4; /*makes square */
+	background-color: rgba(255 255 255 / 0.2);
+	border-radius: 0.75vw;
+	cursor: pointer;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	max-width: 350px;
+}
+.card::before {
+	/*acting on top layer */
+	background: radial-gradient(
+		800px circle at var(--mouse-x) var(--mouse-y),
+		rgba(255 255 255 / 0.1),
+		transparent 40%
+	);
+	z-index: 3;
+}
+.card:hover::before {
+	opacity: 1;
+}
+.card::before,
+.card > .cardBorder {
+	height: 100%;
+	width: 100%;
+	border-radius: inherit;
+	content: "";
+	position: absolute;
+	left: 0px;
+	top: 0px;
+	opacity: 0;
+	transition: opacity 500ms;
+}
+.cardBorder {
+	/*gradient on card thats like => card | cardBorder(radial) | cardContetnt | card::before(raidial) */
+	background: radial-gradient(
+		400px circle at var(--mouse-x) var(--mouse-y),
+		rgba(255, 255, 255, 0.8),
+		transparent 40%
+	);
+	z-index: 1;
+}
+.card > .cardContent {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	position: relative;
+	height: calc(100% - 0.4vw);
+	width: calc(100% - 0.4vw);
+	background: rgba(30 30 30 / 1);
+	margin: 0.2vw;
+	border-radius: inherit;
+	z-index: 2;
+}
+/*end of cards */
+
+.bigNavLink {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	z-index: 4;
+}
+
+.cardImgWrapper > .cardImg {
+	width: 11vw;
+	height: 11vw;
+	max-width: 200px;
+	max-height: 200px;
+}
+.cardImgWrapper {
+	filter: invert(92%); /*pretty much same as title color */
+}
+.infoTitle {
+	font-family: var(--titleFont);
+	color: var(--mainFontColor);
+	font-size: 3vw;
+}
+/*for really big screens */
+@media screen and (min-width: 1500px) {
+	#cards {
+		padding: 50px;
+		display: flex;
+		display: none; /*until I find where to put them */
+		justify-content: space-between;
+	}
+	#cards > .card {
+		width: 350px;
+	}
+	.infoTitle {
+		font-size: 45px;
+	}
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -4,92 +4,6 @@
 			<div class="sizeWarn">
 				This website may have some problems due to your screen width
 			</div>
-			<nav class="navBar">
-				<!--
-			<div class="navWrapper">
-				<router-link to="#start" class="navLinks">stARt</router-link>
-				<router-link to="#about" class="navLinks">About</router-link>
-				<router-link to="#projects" class="navLinks"
-					>pRojects</router-link
-				>
-				<router-link to="#contact" class="navLinks"
-					>coNtAct</router-link
-				>
-			</div>-->
-				<div id="cards">
-					<div class="card">
-						<router-link class="bigNavLink" to="/"></router-link>
-						<div class="cardBorder"></div>
-						<div class="cardContent">
-							<div class="cardImgWrapper">
-								<img
-									src="/house.png"
-									alt=""
-									class="home cardImg"
-								/>
-							</div>
-							<div class="infoWrapper">
-								<div class="infoTitle">Home</div>
-								<div class="infoDescription"></div>
-							</div>
-						</div>
-					</div>
-					<div class="card">
-						<router-link
-							class="bigNavLink"
-							to="#about"
-						></router-link>
-						<div class="cardBorder"></div>
-						<div class="cardContent">
-							<div class="cardImgWrapper">
-								<img src="/info.png" class="about cardImg" />
-							</div>
-							<div class="infoWrapper">
-								<div class="infoTitle">About</div>
-								<div class="infoDescription"></div>
-							</div>
-						</div>
-					</div>
-					<div class="card">
-						<router-link
-							class="bigNavLink"
-							to="#projects"
-						></router-link>
-						<div class="cardBorder"></div>
-						<div class="cardContent">
-							<div class="cardImgWrapper">
-								<img
-									src="/blueprint.png"
-									class="projects cardImg"
-								/>
-							</div>
-							<div class="infoWrapper">
-								<div class="infoTitle">pRojects</div>
-								<div class="infoDescription"></div>
-							</div>
-						</div>
-					</div>
-					<div class="card">
-						<router-link
-							class="bigNavLink"
-							to="#contact"
-						></router-link>
-						<div class="cardBorder"></div>
-						<div class="cardContent">
-							<div class="cardImgWrapper">
-								<img
-									src="/contact.png"
-									class="contact cardImg"
-								/>
-							</div>
-							<div class="infoWrapper">
-								<div class="infoTitle">contAct</div>
-								<div class="infoDescription"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</nav>
 			<div class="titleContainer">
 				<div class="titleWrapper">
 					<span class="mainTitle">HARRy</span>
@@ -187,21 +101,20 @@ body {
 	flex-direction: row;
 }
 .titleContainer {
-	padding: 2vw 0 0 2vw;
+	padding: 1vw 0 0 2vw;
 	position: fixed;
 	top: 0;
 	left: 0;
 	width: 100vw;
 	width: 100dvw;
-	height: calc(21.5vw - var(--scrollY) * 3.3);
-	max-height: 210px;
+	height: calc(21.5vw - var(--scrollY)*1.5);
 	background: linear-gradient(
 		180deg,
 		rgba(20 20 20 / 1) 0%,
-		rgba(20 20 20 / 0.9) 65%,
-		rgba(20 20 20 / 0.75) 78%,
-		rgba(20 20 20 / 0.5) 90%,
-		rgba(20, 20, 20, 0.3) 95%,
+		rgba(20 20 20 / 1) 75%,
+		rgba(20 20 20 / 0.9) 85%,
+		rgba(20 20 20 / 0.7) 92%,
+		rgba(20, 20, 20, 0.5) 95%,
 		rgba(0, 0, 0, 0) 100%
 	);
 }
@@ -242,109 +155,7 @@ body {
 /*Nav */
 /*Nav */
 .navBar {
-	position: relative;
-	display: flex;
-	align-items: center;
-	justify-content: center;
 }
-/*cards */
-/*For Nav bar at start from hyperplexed*/
-#cards {
-	/*display: inline-grid;*/
-	display: none;
-	width: 95vw;
-	width: 95dvw;
-	gap: 1vw;
-	grid-template-columns: repeat(4, 1fr);
-	grid-template-rows: 1fr;
-	padding: 2.5vw;
-}
-#cards:hover > .card > .cardBorder {
-	opacity: 1; /*on hover they all set to clear and set to the coordinate */
-	/*for some reason it doesn't overflow the box */
-	/*doesnt overflow because its a background not its own element numb nuts */
-}
-.card {
-	aspect-ratio: 5/4; /*makes square */
-	background-color: rgba(255 255 255 / 0.2);
-	border-radius: 0.75vw;
-	cursor: pointer;
-	position: relative;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	max-width: 350px;
-}
-.card::before {
-	/*acting on top layer */
-	background: radial-gradient(
-		800px circle at var(--mouse-x) var(--mouse-y),
-		rgba(255 255 255 / 0.1),
-		transparent 40%
-	);
-	z-index: 3;
-}
-.card:hover::before {
-	opacity: 1;
-}
-.card::before,
-.card > .cardBorder {
-	height: 100%;
-	width: 100%;
-	border-radius: inherit;
-	content: "";
-	position: absolute;
-	left: 0px;
-	top: 0px;
-	opacity: 0;
-	transition: opacity 500ms;
-}
-.cardBorder {
-	/*gradient on card thats like => card | cardBorder(radial) | cardContetnt | card::before(raidial) */
-	background: radial-gradient(
-		400px circle at var(--mouse-x) var(--mouse-y),
-		rgba(255, 255, 255, 0.8),
-		transparent 40%
-	);
-	z-index: 1;
-}
-.card > .cardContent {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	position: relative;
-	height: calc(100% - 0.4vw);
-	width: calc(100% - 0.4vw);
-	background: rgba(30 30 30 / 1);
-	margin: 0.2vw;
-	border-radius: inherit;
-	z-index: 2;
-}
-/*end of cards */
-
-.bigNavLink {
-	width: 100%;
-	height: 100%;
-	position: absolute;
-	z-index: 4;
-}
-
-.cardImgWrapper > .cardImg {
-	width: 11vw;
-	height: 11vw;
-	max-width: 200px;
-	max-height: 200px;
-}
-.cardImgWrapper {
-	filter: invert(92%); /*pretty much same as title color */
-}
-.infoTitle {
-	font-family: var(--titleFont);
-	color: var(--mainFontColor);
-	font-size: 3vw;
-}
-
 /*max font stuff (above 1000px) */
 @media screen and (min-width: 1000px) {
 	.mainTitle {
@@ -368,6 +179,10 @@ body {
 		top: calc(-35px + var(--scrollY) / 10);
 		letter-spacing: calc(55px - var(--scrollY) / 2.25);
 	}
+	.titleContainer {
+		padding: 10px 0 0 20px;
+		height: calc(215px - var(--scrollY)*1.5);
+	}
 }
 /*for really small width screens */
 @media screen and (max-width: 300px) {
@@ -376,21 +191,6 @@ body {
 	}
 	.sizeWarn {
 		display: flex;
-	}
-}
-/*for really big screens */
-@media screen and (min-width: 1500px) {
-	#cards {
-		padding: 50px;
-		display: flex;
-		display: none; /*until I find where to put them */
-		justify-content: space-between;
-	}
-	#cards > .card {
-		width: 350px;
-	}
-	.infoTitle {
-		font-size: 45px;
 	}
 }
 </style>
