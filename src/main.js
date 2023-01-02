ea2fdb81618022ff7682a6276c9af1a600a15c87
Duckjpeg <Harry.steven@icloud.com>
2023-01-02 13:59:40 +0000

    scailing title WORKING YIPEE now I need to add the nav fading in and it going below title below 500px

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/styles/title.css'
 
 const app = createApp(App)
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -60,13 +60,23 @@ export default {
 			}
 		},
 		handleOnScroll() {
-			const headerHeight =
-				document.querySelector(".titleContainer").clientHeight;
+			const headerHeight = document.querySelector(".titleContainer").clientHeight;
+			//if height => 22vw => 12vw
 			this.scrollY = window.pageYOffset / 5;
 			if (screen.width < 1000) {
-				//TODO: make it for scalable depending on screen width, SUBRACT a vw I think
+				if ( this.scrollY*0.143 <= 10 ){ //should be *0.22 but js is tweaking
+					document.documentElement.style.setProperty(
+						"--scrollY",
+						`${this.scrollY*0.14}vw`
+					); //should be 10vw ish 
+				} else {
+					document.documentElement.style.setProperty(
+						"--scrollY",
+						`${70*0.143}vw` //should roughly be 10vw (1/7 * 70 to get 10)
+					); //should be 10vw
+				}
 			} else {
-				if (this.scrollY <= 75) {
+				if (this.scrollY <= 70) {
 					document.documentElement.style.setProperty(
 						"--scrollY",
 						`${this.scrollY}px`
@@ -74,7 +84,7 @@ export default {
 				} else {
 					document.documentElement.style.setProperty(
 						"--scrollY",
-						`${75}px`
+						`${70}px`
 					);
 				}
 			}
@@ -83,157 +93,8 @@ export default {
 };
 </script>
 <style>
+/*styles in sections in assets / styles and imported in main.js */
 body {
 	padding-top: 20px;
-}
-/*Title */
-.mainTitle {
-	font-family: var(--titleFont);
-	font-size: calc(11.5vw - var(--scrollY));
-	color: var(--mainFontColor);
-}
-.surnameTitle {
-	font-family: var(--titleFont);
-	font-size: calc(3vw - var(--scrollY) / 5);
-	color: var(--smallTextColor);
-	position: relative;
-	left: 0.5vw;
-	top: calc(-3.5vw + var(--scrollY) / 200);
-	letter-spacing: calc(5.1vw - var(--scrollY) * 0.5);
-}
-.afterSlash {
-	font-family: var(--titleFont);
-	color: var(--mainFontColor);
-	position: relative;
-	font-size: calc(14vw - var(--scrollY));
-	left: 5vw;
-}
-.titleWrapper {
-	display: flex;
-	flex-direction: row;
-}
-.titleContainer {
-	padding: 1vw 0 0 2vw;
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100vw;
-	width: 100dvw;
-	height: calc(21vw - var(--scrollY));
-	background: linear-gradient(
-		180deg,
-		rgba(20 20 20 / 1) 0%,
-		rgba(20 20 20 / 1) 75%,
-		rgba(20 20 20 / 0.9) 85%,
-		rgba(20 20 20 / 0.7) 92%,
-		rgba(20, 20, 20, 0.5) 95%,
-		rgba(0, 0, 0, 0) 100%
-	);
-}
-.slash {
-	width: 1vw;
-	height: calc(15vw - var(--scrollY));
-	max-width: 10px;
-	max-height: 150px;
-	background: linear-gradient(
-		180deg,
-		rgba(20 20 20 /0),
-		rgb(235 235 235 /0.8),
-		rgba(235 235 235 /1),
-		rgb(235 235 235 /0.8),
-		rgba(20 20 20 /0)
-	);
-	position: relative;
-	left: 3.5vw;
-	transform: skew(-30deg);
-}
-
-/*warning banner for screen size */
-.sizeWarn {
-	width: 100vw;
-	width: 100dvw;
-	height: 30px;
-	display: none;
-	font-family: "Inter", sans-serif;
-	font-size: 10px;
-	background: rgba(255, 0, 0, 0.5);
-	position: absolute;
-	left: 0;
-	top: 0;
-	color: var(--mainFontColor);
-}
-
-/*Nav */
-/*Nav */
-/*Nav */
-.navBar {
-}
-/*max font stuff (above 1000px) */
-@media screen and (min-width: 1000px) {
-	.mainTitle {
-		font-size: calc(120px - var(--scrollY) * 0.9);
-	}
-	.slash {
-		left: 35px;
-		height: calc(150px - var(--scrollY));
-	}
-	.afterSlash {
-		position: relative;
-		left: 50px;
-		font-size: calc(150px - var(--scrollY));
-		top: -6px;
-	}
-	.surnameTitle {
-		font-size: calc(30px - var(--scrollY) / 5);
-		left: 5px;
-		top: calc(-35px + var(--scrollY) / 10);
-		letter-spacing: calc(55px - var(--scrollY) / 2.25);
-	}
-	.titleContainer {
-		padding: 10px 0 0 20px;
-		height: calc(210px - var(--scrollY) * 1.5);
-	}
-}
-@media screen and (max-width: 500px) {
-	.titleContainer {
-		left: 3.5vw;
-		height: 21.5vw; /*doesn't shrink since at that scale theres no point */
-	}
-	.slash {
-		top: 6px;
-		height: 15vw;
-	}
-	.mainTitle {
-		position: relative;
-		font-size: 11.5vw;
-		top: 6px;
-	}
-	.surnameTitle {
-		font-size: 3vw;
-		top: -3.5vw;
-		letter-spacing: 5.1vw;
-		top: -2.5vw;
-	}
-	.afterSlash {
-		font-size: 14vw;
-		top: 6px;
-	}
-}
-/*for really small width screens */
-@media screen and (max-width: 300px) {
-	.titleContainer {
-		padding: 5px 0 0 5px;
-	}
-	.titleContainer .afterSlash {
-		position: relative;
-		top: 4px;
-	}
-	.sizeWarn {
-		display: none;
-	}
-	.surnameTitle {
-		position: relative;
-		top: -2vw;
-	}
 }
 </style>
