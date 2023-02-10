d98bd97a3ef952b564b19691299a2aaea135e9ea
Duckjpeg <Harry.steven@icloud.com>
2023-02-10 17:05:04 +0000

    star wars was too hard to upkeep :(

---
+++ src/components/hello.vue
@@ -0,0 +1,61 @@
+<template lang="">
+	<div>
+		<div id="welcomemsg">Hello!</div>
+		<div id="welcomeFiller"></div>
+	</div>
+</template>
+<script>
+export default {
+	mounted() {
+		window.addEventListener("scroll", (event) => this.scrollHello(event));
+		window.addEventListener("resize", () => {
+			document.documentElement.style.setProperty("--veiwPortHeight", `${window.innerHeight}`);
+		});
+		document.documentElement.style.setProperty("--veiwPortHeight", `${window.innerHeight}`);
+	},
+	unmounted() {
+		window.removeEventListener("scroll", this.scrollHello);
+	},
+	methods: {
+		scrollHello(event) {
+			//gah dayum I am smart
+			let scrollHeight = window.pageYOffset;
+			if (scrollHeight > window.innerHeight / 2) {
+				document.querySelector("#welcomemsg").style.display = "none";
+			} else {
+				document.querySelector("#welcomemsg").style.display = "block";
+			}
+			document.documentElement.style.setProperty(
+				"--scrollYnoUnit",
+				`${scrollHeight}` //should roughly be 10vw (1/7 * 70 to get 10)
+			);
+		},
+	},
+};
+</script>
+<style scoped>
+#welcomemsg {
+	font-size: 10vw;
+	color: var(--goldColor);
+	position: fixed;
+	top: 50vh;
+	left: calc(50vw - 100px);
+	font-family: var(--cursiveFont);
+	scale: calc(1 - (var(--scrollYnoUnit) / var(--veiwPortHeight)) * 4 + 1); /*at 50vh down its 0.5% so times 4 = 2 == initial scale  */
+} /*make reactive (size biz) and make it look neat maybe different font for each letter */
+/*TODO: make it max out above 1000px screen width */
+#welcomeFiller {
+	height: 100vh;
+}
+@media screen and (min-width: 1000px) {
+	#welcomemsg {
+		font-size: 100px;
+	}
+}
+@media screen and (max-width: 500px) {
+	#welcomemsg {
+		translate: -50%;
+		left: 50vw;
+	}
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -9,18 +9,17 @@
 			</header>
 			<main id="content">
 				<div id="intro">
-					<div id="welcomemsg">Hello!</div>
-					<div id="filler"></div>
-					<section id="starWarsIntro" class="visibleElements">
-						<starWars />
-					</section>
+					<hello />
 				</div>
 				<div id="mainBody">
 					<section id="about" class="subTitle visibleElements"></section>
 					<section id="contact" class="subTitle visibleElements">
 						<contact />
 					</section>
-					<div class="cheese" style="width:50px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates quam cumque at quo ipsum necessitatibus laboriosam earum error tempora, molestias ut laborum alias distinctio voluptate odit, voluptatibus magni quaerat voluptas!</div>
+					<div class="cheese" style="width: 50px">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates quam cumque at quo ipsum necessitatibus laboriosam earum error
+						tempora, molestias ut laborum alias distinctio voluptate odit, voluptatibus magni quaerat voluptas!
+					</div>
 				</div>
 			</main>
 		</section>
@@ -30,64 +29,19 @@
 import mouse from "../components/mouse.vue";
 import topPage from "../components/topPage.vue";
 import navBar from "../components/navBar.vue";
-import starWars from "../components/starWars.vue";
 import contact from "../components/contact.vue";
+import hello from "../components/hello.vue";
 export default {
 	components: {
 		mouse,
 		topPage,
 		navBar,
-		starWars,
 		contact,
+		hello,
 	},
-	mounted() {
-		window.addEventListener("scroll", (event) => this.scrollBiz(event));
-		window.addEventListener("resize", () => this.fillspacing());
-		this.fillspacing();
-	},
-	unmounted() {
-		//document.removeEventListener(card.onmousemove);
-		window.removeEventListener("scroll", this.scrollBiz);
-		window.removeEventListener("resize", () => this.fillspacing());
-	},
-	methods: {
-		scrollBiz(event) {
-			//basically timelining
-			let scrollHeight = window.pageYOffset;
-			if (scrollHeight > 400) {
-				document.querySelector("#welcomemsg").style.display = "none";
-			} else {
-				document.querySelector("#welcomemsg").style.display = "block";
-			}
-			if (
-				parseInt(document.querySelector("#filler").style.height) +
-					document.querySelector("#filler").offsetTop -
-					document.querySelector("#titleComp").getBoundingClientRect().height * 2 <
-				window.pageYOffset
-			) {
-				document.querySelector("#starWarsIntro").style.opacity = 0;
-			} else {
-				document.querySelector("#starWarsIntro").style.opacity = 1;
-			}
-		},
-		fillspacing() {
-			let firstDiv = document.querySelector("#filler");
-			let starWarsHeight = document.querySelector("#starWarsContentBody");
-			if (window.innerWidth > 500) {
-				firstDiv.style.height = `${
-					starWarsHeight.clientHeight / (0.8 - 1 / (window.innerWidth / 75)) //so it get bigger as screen size reduces
-				}px`;
-			} else if (window.innerWidth > 350) {
-				firstDiv.style.height = `${
-					starWarsHeight.clientHeight / (0.2 + window.innerWidth / 1700) //so it get bigger as screen size reduces
-				}px`;
-			} else {
-				firstDiv.style.height = `${
-					starWarsHeight.clientHeight / (0.2 + window.innerWidth / 1500) //so it get bigger as screen size reduces
-				}px`;
-			}
-		},
-	},
+	mounted() {},
+	unmounted() {},
+	methods: {},
 };
 </script>
 <style>
@@ -96,23 +50,6 @@ export default {
 }
 /*TODO: maybe add a lil something fly around on scroll */
 
-main #welcomemsg {
-	font-size: 10vw;
-	color: var(--goldColor);
-	position: fixed;
-	top: calc(45vh);
-	font-family: var(--cursiveFont);
-	scale: calc(1 - (var(--scrollYnoUnit) / 200) + 1);
-} /*make reactive (size biz) and make it look neat maybe different font for each letter */
-/*TODO: make it max out above 1000px screen width */
-
-#filler {
-	width: calc(100vw - 100px);
-}
-#starWarsIntro {
-	top: -8vw;
-	position: fixed;
-}
 #mainBody {
 	position: relative;
 	left: 100px;
